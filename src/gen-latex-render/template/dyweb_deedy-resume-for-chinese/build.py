c5e351e83d43e4c0915202f9ce891582826a4816
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-10-03 04:22:25 +0800

    fix(build.py): succeed in font hash check

--- src/gen-latex-render/template/dyweb_deedy-resume-for-chinese/build.py
+++ src/gen-latex-render/template/dyweb_deedy-resume-for-chinese/build.py
@@ -5,6 +5,8 @@ font_hash_checklist = {"afaffdfaf": "68e109f0f40ca72a15e05cc22786f8e6"}
 
 
 def get_fonts(font_weighted_name: str):
+    # https://mirrors.tuna.tsinghua.edu.cn/adobe-fonts/source-han-serif/OTF/SimplifiedChinese/
+    # https://mirrors.tuna.tsinghua.edu.cn/adobe-fonts/source-han-sans/OTF/SimplifiedChinese/
     pass
 
 
@@ -12,26 +14,30 @@ def check_crc32(file_path: str, font_weighted_name: str):
     import hashlib
 
     flag_ensure_font_original = True
-    temp_hash = hashlib.md5()
-    temp_hash.update(b"HelloWorld")
+    flag_build_dict=False
     try:
-        print(file_path)
-        file_raw = open(file_path, "b")
+        # print(file_path)
+        file_raw = open(file_path, "rb")
     except:
         print("[ERROR]Failed to open font file.")
         return -1
-    temp_hash.update(file_raw)
+    temp_hash = hashlib.md5()
+    temp_hash.update(b"HelloWorld")
+    temp_hash.update(file_raw.read())
     hash_result = str(temp_hash.hexdigest())
-    print(hash_result)
+    if flag_build_dict==True:
+        print("\""+font_weighted_name+"\":\""+hash_result+"\",") 
+        return 1
     if hash_result == "68e109f0f40ca72a15e05cc22786f8e6":
         print("[ERROR]Failed to calc MD5")  # "HelloWorld"'s MD5 is above
     else:
-        if font_hash_checklist.get(font_hash_checklist) != hash_result:
+        if font_hash_checklist.get(font_weighted_name) != hash_result:
             print("[WARN]Font file was not original")
             if flag_ensure_font_original == True:
                 get_fonts(font_weighted_name=font_weighted_name)
         else:
             print("[INFO]" + font_weighted_name + " passed check.")
+    return 0
 
 
 def pre_check_fonts():
